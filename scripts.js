66de4b692666e19e0791377c2bb0cca7d1145f0e
Infinity Events <infinityeventsit@gmail.com>
2024-08-08 15:19:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,6 +29,7 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     menu.style.animation = 'fadeInOut 0.5s ease-in-out';
 });
 
+
 // FAQ section toggle
 const faqItems = document.querySelectorAll('.faq-item');
 
@@ -44,8 +45,65 @@ faqItems.forEach(item => {
             answer.style.display = 'block';
             span.style.transform = 'rotate(180deg)';
         }
+
+        answer.style.animation = 'fadeInOut 0.5s ease-in-out';
     });
 });
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+    cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+    nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
 
 // Responsive table for the schedule section
 const table = document.querySelector('table');
