4d2ed83c4cadf5f9b753e046c0a9e18ccaa31fbb
AndryX-08 <90835105+AndryX-08@users.noreply.github.com>
2026-08-05 15:25:56 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -457,7 +457,7 @@ async function loadMyTickets(){
 
     const tickets = await response.json();
     console.log("TICKET RICEVUTI:", tickets);
-    
+
     const container =
     document.getElementById("my-ticket-list");
 
@@ -492,54 +492,4 @@ async function loadMyTickets(){
     ticket.code
     );
     });
-}
-
-//TICKETS
-window.loadMyTickets = async function(){
-const user = window.auth.currentUser;
-if(!user){
-    return;
-}
-const token = await user.getIdToken();
-const response = await fetch(
-"https://infinity-eventos-api.onrender.com/tickets/user/me",
-{
-headers:{
-Authorization:`Bearer ${token}`
-}
-}
-);
-
-const tickets = await response.json();
-
-const box=document.getElementById(
-"my-ticket-list"
-);
-if(!box)return;
-
-box.innerHTML="";
-
-tickets.forEach(ticket=>{
-box.innerHTML+=`
-<article class="ticket-card">
-    <div class="ticket-top">
-        <h3>${ticket.type}</h3>
-        <span class="price">
-        €${ticket.price}
-        </span>
-    </div>
-    <p>
-    Codice:
-    <b>${ticket.code}</b>
-    </p>
-    <div id="qr-${ticket.id}">
-    </div>
-</article>
-`;
-
-QRCode.toCanvas(
-document.getElementById(`qr-${ticket.id}`),
-ticket.code
-);
-});
 }
