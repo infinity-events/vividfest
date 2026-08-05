4aad1b299d416a55354b1130d67806df85120fe2
AndryX-08 <90835105+AndryX-08@users.noreply.github.com>
2026-08-05 11:43:11 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -29,13 +29,13 @@
         const emailSpan = document.getElementById('userEmail');
         const emailFromSession = sessionStorage.getItem('userEmail');
 
-        const auth = getAuth(app);
-
         window.auth = auth; // Rende auth accessibile globalmente
         console.log("AUTH GLOBALE:", window.auth);
         window.db = db; // Rende db accessibile globalmente 
-        window.provider = provider; // Rende provider accessibile globalmente 
+        window.provider = new GoogleAuthProvider(); // Rende provider accessibile globalmente 
         window.signInWithPopup = signInWithPopup; // Rende signIn accessibile globalmente 
+
+        const auth = getAuth(app);
 
         if (emailFromSession) {
     emailSpan.textContent = emailFromSession;
@@ -69,172 +69,6 @@
 
     const API_URL="https://infinity-eventos-api.onrender.com";
     const FESTIVAL_ID="438e5467-925a-40cd-bfdb-1750795e35a2";
-    async function loadTickets(){
-    try{
-        const response = await fetch(
-        `${API_URL}/ticket-category/${FESTIVAL_ID}`
-    );
-
-    const categories = await response.json();
-    console.log("Categorie ricevute:", categories);
-    const container=document.getElementById("ticket-list");
-    if(!container){
-        console.error("Elemento ticket-list non trovato");
-    return;
-    }
-
-    window.buyTicket = async function(categoryId){
-
-    if(!window.auth){
-
-    console.error("Firebase non inizializzato");
-
-    return;
-
-    }
-
-
-    let user = window.auth.currentUser;
-
-
-    if(!user){
-
-    await window.signInWithPopup(
-    window.auth,
-    window.provider
-    );
-
-    }
-
-    user = window.auth.currentUser;
-
-    const token =
-    await user.getIdToken();
-
-    console.log("TOKEN:",token);
-    const response = await fetch(
-    "https://infinity-eventos-api.onrender.com/tickets/purchase/"+categoryId,
-    {
-    method:"POST",
-    headers:{
-    Authorization:"Bearer "+token
-    }
-
-    });
-
-    const ticket = await response.json();
-    console.log("Ticket creato:",ticket);
-}
-
-    categories.forEach(category=>{
-
-
-    const available =
-    category.quantity-category.sold;
-
-
-    container.innerHTML += `
-
-    <article class="ticket-card ${category.type==="VIP" ? "featured" : ""}">
-
-    <div class="ticket-top">
-
-    <h3>${category.name}</h3>
-
-    <span class="price">
-    €${category.price}
-    </span>
-
-    </div>
-
-
-    <ul class="ticket-benefits">
-
-    <li>
-    <span class="check-icon">
-    <img src="img/check.png">
-    </span>
-    Accesso agli stage
-    </li>
-
-
-    <li>
-    <span class="check-icon">
-    <img src="img/check.png">
-    </span>
-    Bancarelle cibo
-    </li>
-
-
-    <li class="${category.type==="VIP" ? "" : "removed"}">
-
-    <span class="check-icon">
-
-    <img src="img/${category.type==="VIP" ? "check.png" : "remove.png"}">
-
-    </span>
-
-    Accesso area VIP dedicata
-
-    </li>
-
-
-    <li class="${category.type==="BACKSTAGE" ? "" : "removed"}">
-
-    <span class="check-icon">
-
-    <img src="img/${category.type==="BACKSTAGE" ? "check.png" : "remove.png"}">
-
-    </span>
-
-    Tour backstage
-
-    </li>
-
-
-    </ul>
-
-
-    <div class="ticket-button-container">
-
-    <button class="buy-ticket-button" onclick="buyTicket('${category.id}')">
-
-    <span>
-    Acquista Ora
-    <img class="arrow-icon" src="img/arrow.png">
-    </span>
-
-    </button>
-
-
-    </div>
-
-
-    </article>
-
-    `;
-
-    });
-
-
-    }
-    catch(error){
-
-    console.error(
-    "Errore caricamento ticket:",
-    error
-    );
-
-    }
-
-    }
-
-
-    document.addEventListener(
-    "DOMContentLoaded",
-    loadTickets
-    );
-
 async function loadMyTickets(){
     const user=auth.currentUser;
     if(!user)return;
